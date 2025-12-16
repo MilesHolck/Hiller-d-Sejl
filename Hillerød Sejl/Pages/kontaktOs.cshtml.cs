090aef067896c7ce203a3316d204ef85512f6455
camil <camilla-holck@hotmail.com>
2025-12-16 14:05:44 +0100

    ændringer

---
+++ Hillerød Sejl/Pages/kontaktOs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hillerød_Sejl.Pages
+{
+    public class kontaktOsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
